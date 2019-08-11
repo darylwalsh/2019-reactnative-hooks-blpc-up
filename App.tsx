59e7b02fda1c3a21405f7d5cb09932d3316a0642
Daryl <daryl@blpc.us>
2019-08-10 20:52:03 -0400

    feat(list_screen): add skeleton list screen

--- App.tsx
+++ App.tsx
@@ -2,14 +2,16 @@ import { createAppContainer, createStackNavigator } from 'react-navigation'
 
 import ComponentsScreen from './src/screens/ComponentsScreen'
 import HomeScreen from './src/screens/HomeScreen'
+import ListScreen from './src/screens/ListScreen'
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: 'Components',
+    initialRouteName: 'List',
     defaultNavigationOptions: {
       title: 'App',
     },
